720c1952b1632d2e2dcf859055ee8894fb77ad26
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-08-16 00:00:32 +0000

    [fix]: made the format integer

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
     for i in range(1, length + 1):
         idx = length - i
-        print('{}'.format(my_list[idx]))
+        print('{:d}'.format(my_list[idx]))
