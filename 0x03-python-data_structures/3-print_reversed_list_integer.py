2f07963e3980c982bf4ead0bc74482e60d0c3438
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-08-16 00:06:11 +0000

    [fix]: add a conditional statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
-    for i in range(1, length + 1):
-        idx = length - i
-        print('{:d}'.format(my_list[idx]))
+    if not my_list:
+        pass
+    else:
+        length = len(my_list)
+        for i in range(1, length + 1):
+            idx = length - i
+            print('{:d}'.format(my_list[idx]))
