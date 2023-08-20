26f649c430c171645e6963541ee4ae4014c32313
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-08-20 22:25:33 +0000

    [fix]: changed the code

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,7 +2,12 @@
 def only_diff_elements(set_1, set_2):
     new_set = set()
 
-    new_set.update(set_1)
-    new_set.update(set_2)
+    for element in set_1:
+        if element not in set_2:
+            new_set.add(element)
+
+    for element in set_2:
+        if element not in set_1:
+            new_set.add(element)
 
     return (new_set)
