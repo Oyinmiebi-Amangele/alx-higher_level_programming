9b54ed913fc7af3ea52bae99e8b080028eebc301
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-09-04 22:00:40 +0000

    [fix]

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,26 +1,54 @@
 #!/usr/bin/python3
-"""A class that defines a Rectangle"""
+"""
+
+This module is composed by a class that defines a Rectangle
+
+
+"""
 
 
 class Rectangle:
-    """Represent the Rectangle"""
+    """ Class that defines a rectangle """
 
-    def __init___(self, width=0, height=0):
-        """Initializing the rectangle.
+    def __init__(self, width=0, height=0):
+        """ Method that initializes the instance
+
         Args:
-            width(int): width of the rectangle.
-            height(int): height of the rectangle.
+            width: rectangle width
+            height: rectangle height
+
+
         """
+
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get/set the property of the width"""
+        """ method that returns the value of the width
+
+        Returns:
+            rectangle width
+
+
+        """
+
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ method that defines the width
+
+        Args:
+            value: width
+
+        Raises:
+            TypeError: if width is not an integer
+            ValueError: if width is less than zero
+
+
+        """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -29,18 +57,37 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the property of the height"""
+        """ method that returns the value of the height
+
+        Returns:
+            rectangle height
+
+
+        """
+
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ method that defines the height
+
+        Args:
+            value: height
+
+        Raises:
+            TypeError: if height is not an integer
+            ValueError: if height is less than zero
+
+
+        """
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-     def area(self):
+    def area(self):
         """ Method that calculates the Rectangle area
 
         Returns:
