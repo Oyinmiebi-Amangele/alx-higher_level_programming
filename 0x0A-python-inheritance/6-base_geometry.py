37cfae57766aca2a38d7c5d4dfea3e73679f7170
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-09-13 02:38:07 +0000

    [feature]:defining the exception of area of basegeometry not defined

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defining a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def area(self):
+        """Area of the BaseGeometry"""
+        raise Exception("area() is not implemented")
