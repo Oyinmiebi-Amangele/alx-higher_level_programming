67ac839fffa3764ec81cba63cd86d44bfd51e2bf
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-10-09 23:44:54 +0000

    [fix]:created changes

--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Creating a BaseGeometry class"""
-
-
-class BaseGeometry:
-    """An empty class"""
-    pass
--- 0x0A-python-inheritance/6-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Defining a class BaseGeometry"""
-
-
-class BaseGeometry:
-    """Empty class"""
-    def area(self):
-        """Area of the BaseGeometry"""
-        raise Exception("area() is not implemented")
