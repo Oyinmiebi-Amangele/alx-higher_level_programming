90e98ce0f19adb03bbd7a384608295bef0bf3b87
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-10-10 02:07:05 +0000

    [feature]:created a class Rectangle and initialized it

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Creating a class Rectangle from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defining the properties of a Rectangle."""
+
+    def __init__(self, width, height):
+        """initializing the width and height"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
