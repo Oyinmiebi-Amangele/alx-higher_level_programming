5e76bd33987a7c81db00358a5917033fa5966724
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-10-10 01:03:59 +0000

    [feature]:created an instance method area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Creating a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+        Defining the properties of the BaseGeometry
+    """
+
+    def area(self):
+        """
+            area not implemented
+        """
+        raise Exception("area() is not implemented)
